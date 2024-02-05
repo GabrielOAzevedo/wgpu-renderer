4914b625a8616acbf2a666aa1e843a929d7c492d
Gabriel Azevedo <goazevedo@icloud.com>
2024-02-04 22:03:17 -0300

    split adapter code

--- lib/adapter.h
+++ lib/adapter.h
@@ -1,4 +1,8 @@
+#ifndef WEBGPU_ADAPTER_H_
+#define WEBGPU_ADAPTER_H_
 #include <webgpu/webgpu.h>
 
 WGPUAdapter requestAdapter(WGPUInstance instance,
                            WGPURequestAdapterOptions const *options);
+
+#endif // WEBGPU_ADAPTER_H_
